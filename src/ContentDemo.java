a35b6bec3bdad8d1f8533ab1242a56f81489cab4
Ehsanullah Alimy <e.alimy2008@gmail.com>
2025-12-30 10:21:53 +0500

    Initial commit

---
+++ src/ContentDemo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ContentDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<ContentItem> items = new ArrayList<>();
+
+        items.add(new VideoLecture("Java OOP Basics", 2024, 90, "HD"));
+        items.add(new VideoLecture("Advanced Java", 2022, 120, "4K"));
+
+        items.add(new PodcastEpisode("Tech Talk", 2023, 45, "Alice"));
+        items.add(new PodcastEpisode("Code Stories", 2021, 60, "Bob"));
+
+        int currentYear = java.time.Year.now().getValue();
+
+        for (ContentItem item : items) {
+            System.out.println(
+                    item + " | licenseCost=" + item.getLicenseCost(currentYear)
+            );
+
+            if (item instanceof Downloadable) {
+                Downloadable d = (Downloadable) item;
+                d.download();
+                System.out.println("Max downloads/day: " + d.getMaxDownloadsPerDay());
+            }
+
+            System.out.println("-----------------------------");
+        }
+    }
+}
